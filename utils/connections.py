e3c1863dfd50bb36cc00f72fc0c1b94cae3a2519
Stewart Morawetz <stewm078@gmail.com>
2024-04-09 15:40:52 -0400

    add triplets of connected spins in 1d for 3 site controls

--- utils/connections.py
+++ utils/connections.py
@@ -8,6 +8,16 @@ def neighbours_1d(N, boundary_conds):
         raise ValueError("{0} is not a valid boundary condition".format(boundary_conds))
 
 
+def triplets_1d(N, boundary_conds):
+    """Get all triplets of neighbouring spins (3 in a row) in a 1D chain"""
+    if boundary_conds == "periodic":
+        return [[i, (i + 1) % N, (i + 2) % N] for i in range(N)]
+    elif boundary_conds == "open":
+        return [[i, i + 1, i + 2] for i in range(N - 2)]
+    else:
+        raise ValueError("{0} is not a valid boundary condition".format(boundary_conds))
+
+
 def all_connections_1d(N, boundary_conds):
     """Find interactions between all pairs of sites in a 1D chain, and the
     distance between them (works for PBC and OBC)"""

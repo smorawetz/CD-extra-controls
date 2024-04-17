6cf3bd9c3b43496304234ad8defbad89b127ebbe
Stewart Morawetz <stewm078@gmail.com>
2024-04-17 10:43:53 -0400

    MINOR: add models to data dicts

--- utils/make_dict_data.py
+++ utils/make_dict_data.py
@@ -1,6 +1,13 @@
+from ..models.spinhalf_1d.TFIM_annealing_1d import TFIM_1D
+
+models_dict = {"TFIM_1D": TFIM_1D}
 param_names_dict = {
     "TFIM_1D": "J{0}_hx{1}",
 }
+
+with open("../dicts/models.pkl", "wb") as f:
+    pickle.dump(models_dict, f)
+
 
 with open("../dicts/model_param_names.pkl", "wb") as f:
     pickle.dump(param_names_dict, f)

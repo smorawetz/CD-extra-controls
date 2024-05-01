2ec3330ce6a31ff8f8824bf84b25052051cfc926
Stewart Morawetz <stewm078@gmail.com>
2024-05-01 09:30:09 -0400

    iterative optimization from infinite T

---
+++ run/run_iterative_optimization.py
@@ -0,0 +1,173 @@
+import os
+import sys
+
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+
+from matplotlib.ticker import MaxNLocator
+
+sys.path.append(os.environ["CD_CODE_DIR"])
+
+from cd_protocol import CD_Protocol
+from tools.build_ham import build_ham
+from tools.calc_coeffs import save_alphas, save_lanc_coeffs, save_gammas
+from tools.lin_alg_calls import calc_fid
+from tools.schedules import LinearSchedule, SmoothSchedule
+from tools.symmetries import get_symm_op
+from utils.file_naming import make_coeffs_fname, make_evolved_wfs_fname
+from utils.grid_utils import get_coeffs_interp
+
+model_name = "TFIM_1D"
+Ns = 10
+H_params = [1, 1]
+boundary_conds = "periodic"
+
+symms = ["translation_1d", "spin_inversion"]
+symms_args = [[Ns], [Ns]]
+symm_nums = [0, 0]
+symmetries = {
+    symms[i]: (get_symm_op(symms[i], *symms_args[i]), symm_nums[i])
+    for i in range(len(symms))
+}
+target_symmetries = symmetries
+
+tau = 0.01
+sched = SmoothSchedule(tau)
+
+ctrls = []
+couplings = []
+couplings_args = []
+
+agp_order = 4
+AGPtype = "krylov"
+# AGPtype = "commutator"
+norm_type = "trace"
+# norm_type = "ground_state"
+
+grid_size = 1000
+
+coeffs_sched = LinearSchedule(1)  # always use tau = 1 for grid save
+coeffs_append_str = "normal"
+wfs_append_str = "iterative"
+
+# load Hamiltonian and initial coefficients from ground state
+ham = build_ham(
+    model_name,
+    Ns,
+    H_params,
+    boundary_conds,
+    agp_order,
+    norm_type,
+    sched,
+    symmetries=symmetries,
+    target_symmetries=symmetries,
+)
+coeffs_fname = make_coeffs_fname(
+    ham,
+    model_name,
+    ctrls,
+    AGPtype,
+    norm_type,
+    grid_size,
+    coeffs_sched,  # need separate coeff sched!!
+    coeffs_append_str,
+)
+# load relevant coeffs for AGP
+if AGPtype == "commutator":
+    tgrid = np.loadtxt("coeffs_data/{0}_alphas_tgrid.txt".format(coeffs_fname))
+    alphas_grid = np.loadtxt(
+        "coeffs_data/{0}_alphas_grid.txt".format(coeffs_fname), ndmin=2
+    )
+    ham.alphas_interp = get_coeffs_interp(coeffs_sched, sched, tgrid, alphas_grid)
+elif AGPtype == "krylov":
+    tgrid = np.loadtxt("coeffs_data/{0}_lanc_coeffs_tgrid.txt".format(coeffs_fname))
+    lgrid = np.loadtxt(
+        "coeffs_data/{0}_lanc_coeffs_grid.txt".format(coeffs_fname), ndmin=2
+    )
+    gammas_grid = np.loadtxt(
+        "coeffs_data/{0}_gammas_grid.txt".format(coeffs_fname), ndmin=2
+    )
+    ham.lanc_interp = get_coeffs_interp(coeffs_sched, sched, tgrid, lgrid)
+    ham.gammas_interp = get_coeffs_interp(coeffs_sched, sched, tgrid, gammas_grid)
+else:
+    raise ValueError(f"AGPtype {AGPtype} not recognized")
+
+# loop until fid is converged
+fid = 0
+last_fid = -1
+fids = []
+
+wfs_fname = None  # unnecessary since not save wfs at each step
+i = 1  # index to track iteration number
+while abs(fid - last_fid) > 1e-4 and i < 10:
+    cd_protocol = CD_Protocol(
+        ham, AGPtype, ctrls, couplings, couplings_args, sched, grid_size
+    )
+    init_state = ham.get_init_gstate()
+    targ_state = ham.get_targ_gstate()
+    t_data, wf_data = cd_protocol.matrix_evolve(
+        init_state, wfs_fname, save_states=False
+    )
+    wf_interp = scipy.interpolate.interp1d(
+        t_data, wf_data, axis=0, fill_value="extrapolate"
+    )
+    final_state = wf_data[-1, :]
+    last_fid = fid
+    fid = calc_fid(targ_state, final_state)
+    fids.append(fid)
+    print(f"step {i} fidelity is ", fid)
+    print("final state is ", final_state)
+    fname = make_coeffs_fname(
+        ham,
+        model_name,
+        ctrls,
+        AGPtype,
+        norm_type,
+        grid_size,
+        coeffs_sched,
+        "iter_step_{0}".format(i),
+    )
+    if AGPtype == "commutator":
+        tgrid, alpha_grid = save_alphas(
+            ham, fname, grid_size, sched, agp_order, norm_type, gs_func=wf_interp
+        )
+        ham.alphas_interp = scipy.interpolate.interp1d(
+            tgrid, alpha_grid, axis=0, fill_value="extrapolate"
+        )
+    elif AGPtype == "krylov":
+        lanc_tgrid, lanc_grid = save_lanc_coeffs(
+            ham, fname, grid_size, sched, agp_order, norm_type, gs_func=wf_interp
+        )
+        ham.lanc_interp = scipy.interpolate.interp1d(
+            lanc_tgrid, lanc_grid, axis=0, fill_value="extrapolate"
+        )
+        tgrid, gammas_grid = save_gammas(
+            ham, fname, grid_size, sched, agp_order, norm_type, gs_func=wf_interp
+        )
+        ham.gammas_interp = scipy.interpolate.interp1d(
+            tgrid, gammas_grid, axis=0, fill_value="extrapolate"
+        )
+    else:
+        raise ValueError(f"AGPtype {AGPtype} not recognized")
+    i += 1
+
+
+wfs_fname = make_evolved_wfs_fname(
+    ham,
+    model_name,
+    ctrls,
+    AGPtype,
+    norm_type,
+    grid_size,
+    sched.tau,
+    wfs_append_str,
+)
+t_data, wf_data = cd_protocol.matrix_evolve(init_state, wfs_fname, save_states=True)
+final_state = wf_data[-1, :]
+print("fidelity of final iterated state is ", calc_fid(targ_state, final_state))
+
+fig, ax = plt.subplots()
+ax.plot(fids, "o")
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+plt.savefig(f"fids_vs_iter_start_infT_{AGPtype}_ord{agp_order}.png")

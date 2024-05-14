052790e61f7c047cd67d5cbe0bdc342b54d4ea44
Stewart Morawetz <stewm078@gmail.com>
2024-05-14 15:45:49 -0400

    make fname kwarg of save coeffs

--- tools/calc_coeffs.py
+++ tools/calc_coeffs.py
@@ -31,7 +31,7 @@ def mod_tgrid(t_grid):
 ## use linear schedule, and do once with a larger grid size
 ## which should hopefully avoid any problems
 def calc_alphas_grid(
-    ham, fname, grid_size, sched, agp_order, norm_type, gs_func=None, save=True
+    ham, grid_size, sched, agp_order, norm_type, gs_func=None, save=True, fname=None
 ):
     """Compute the coefficients for the AGP in the commutator ansatz
     on a grid covering the whole protocol, and save them to a file.
@@ -39,13 +39,13 @@ def calc_alphas_grid(
     evolution type problems
     Parameters:
         ham (Hamiltonian_CD):       Counterdiabatic Hamiltonian of interest
-        fname (str):                Name of file to store in
         grid_size (int):            Number of time steps to take
         sched (Schedule):           Schedule object that encodes $\lambda(t)$
         agp_order (int):            Order of the AGP to compute
         norm_type (str):            Either "trace" or "ground_state" for the norm
         gs_func (function):         Function to compute the ground state of the Hamiltonian
         save (bool):                Whether to save the coefficients to a file
+        fname (str):                Name of file to store in
     """
     lam_grid = np.linspace(0, 1, grid_size)
     t_grid = mod_tgrid(sched.get_t(lam_grid))
@@ -53,27 +53,27 @@ def calc_alphas_grid(
     for i in range(grid_size):
         gstate = gs_func(t_grid[i]) if gs_func is not None else None
         alphas_grid[i, :] = ham.calc_alphas(t_grid[i], norm_type, gstate=gstate)
-    if save:
+    if save and fname is not None:
         np.savetxt("coeffs_data/{0}_alphas_tgrid.txt".format(fname), t_grid)
         np.savetxt("coeffs_data/{0}_alphas_grid.txt".format(fname), alphas_grid)
     return t_grid, alphas_grid
 
 
 def calc_lanc_coeffs_grid(
-    ham, fname, grid_size, sched, agp_order, norm_type, gs_func=None, save=True
+    ham, grid_size, sched, agp_order, norm_type, gs_func=None, save=True, fname=None
 ):
     """Compute the Lanczos coefficients on a grid covering the whole protocol,
     and save them to a file.  This can later be indexed and translated for repeated use
     in e.g. evolution
     Parameters:
         ham (Hamiltonian_CD):       Counterdiabatic Hamiltonian of interest
-        fname (str):                Name of file to store in
         grid_size (int):            Number of time steps to take
         sched (Schedule):           Schedule object that encodes $\lambda(t)$
         agp_order (int):            Order of the AGP to compute
         norm_type (str):            Either "trace" or "ground_state" for the norm
         gs_func (function):         Function to compute the ground state of the Hamiltonian
         save (bool):                Whether to save the coefficients to a file
+        fname (str):                Name of file to store in
     """
     lam_grid = np.linspace(0, 1, grid_size)
     t_grid = mod_tgrid(sched.get_t(lam_grid))
@@ -81,14 +81,14 @@ def calc_lanc_coeffs_grid(
     for i in range(grid_size):
         gstate = gs_func(t_grid[i]) if gs_func is not None else None
         lanc_grid[i, :] = ham.calc_lanc_coeffs(t_grid[i], norm_type, gstate=gstate)
-    if save:
+    if save and fname is not None:
         np.savetxt("coeffs_data/{0}_lanc_coeffs_tgrid.txt".format(fname), t_grid)
         np.savetxt("coeffs_data/{0}_lanc_coeffs_grid.txt".format(fname), lanc_grid)
     return t_grid, lanc_grid
 
 
 def calc_gammas_grid(
-    ham, fname, grid_size, sched, agp_order, norm_type, gs_func=None, save=True
+    ham, grid_size, sched, agp_order, norm_type, gs_func=None, save=True, fname=None
 ):
     """Compute the coefficients for the AGP in the Krylov space ansatz
     on a grid covering the whole protocol, and save them to a file.
@@ -96,20 +96,20 @@ def calc_gammas_grid(
     evolution type problems
     Parameters:
         ham (Hamiltonian_CD):       Counterdiabatic Hamiltonian of interest
-        fname (str):                Name of file to store in
         grid_size (int):            Number of time steps to take
         sched (Schedule):           Schedule object that encodes $\lambda(t)$
         agp_order (int):            Order of the AGP to compute
         norm_type (str):            Either "trace" or "ground_state" for the norm
         gs_func (function):         Function to compute the ground state of the Hamiltonian
         save (bool):                Whether to save the coefficients to a file
+        fname (str):                Name of file to store in
     """
     lam_grid = np.linspace(0, 1, grid_size)
     t_grid = mod_tgrid(sched.get_t(lam_grid))
     gammas_grid = np.zeros((grid_size, agp_order))
     for i in range(grid_size):
         gammas_grid[i, :] = ham.calc_gammas(t_grid[i])
-    if save:
+    if save and fname is not None:
         np.savetxt("coeffs_data/{0}_gammas_tgrid.txt".format(fname), t_grid)
         np.savetxt("coeffs_data/{0}_gammas_grid.txt".format(fname), gammas_grid)
     return t_grid, gammas_grid

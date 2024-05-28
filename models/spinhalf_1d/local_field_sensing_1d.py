68a2f99427e52f4b41101d23e4f397c1fb35f338
Stewart Morawetz <stewm078@gmail.com>
2024-05-28 18:11:32 -0400

    added local model with constant longitudinal field for sensing

---
+++ models/spinhalf_1d/local_field_sensing_1d.py
@@ -0,0 +1,93 @@
+import os
+import sys
+
+import quspin
+
+sys.path.append(os.environ["CD_CODE_DIR"])
+
+from .spinhalf_1d import SpinHalf_1D
+from tools.ham_couplings import turn_off_coupling, turn_on_coupling
+from tools.connections import neighbours_1d
+
+
+class Local_Field_Sensing_1D(SpinHalf_1D):
+    """Class for a localling interacting spin chain, with an annealing
+    transverse field and a symmetry-breaking longitudinal field, whose
+    e.g. strength is detected by measuring the state"""
+
+    def __init__(
+        self,
+        Ns,
+        H_params,
+        boundary_conds,
+        agp_order,
+        norm_type,
+        schedule,
+        symmetries={},
+        target_symmetries={},
+    ):
+
+        J, hx, hz = H_params
+        pairs = neighbours_1d(Ns, boundary_conds)
+        self.J_terms = [[-J, *pairs[i]] for i in range(len(pairs))]
+        self.hx_terms = [[-hx, i] for i in range(Ns)]
+        self.flipped_hx_terms = [[hx, i] for i in range(Ns)]
+        self.hz_terms = [[-hz, i] for i in range(Ns)]
+
+        super().__init__(
+            Ns,
+            H_params,
+            boundary_conds,
+            agp_order,
+            norm_type,
+            schedule,
+            symmetries=symmetries,
+            target_symmetries=target_symmetries,
+        )
+
+    def build_H0(self):
+        """Build QuSpin Hamiltonian for H0, which is component being
+        "turned on" in annealing problem"""
+        # s = [["zz", self.J_terms], ["z", self.hz_terms]]
+        s = [["zz", self.J_terms], ["z", self.hz_terms]]
+        d = []
+        return quspin.operators.hamiltonian(s, d, basis=self.basis, **self.checks)
+
+    def build_H1(self):
+        """Method specific to this spin model to calculate
+        the bare Hamiltonian (no controls or AGP)
+        """
+        # s = [["x", self.hx_terms]]
+        s = [["x", self.hx_terms], ["z", self.hz_terms]]
+        d = []
+        return quspin.operators.hamiltonian(s, d, basis=self.basis, **self.checks)
+
+    def build_bare_H(self):
+        """Method specific to this spin model to calculate
+        the bare Hamiltonian (no controls or AGP)
+        """
+        s = [["z", self.hz_terms]]
+        d = [
+            ["zz", self.J_terms, turn_on_coupling, [self.schedule]],
+            # ["z", self.hz_terms, turn_on_coupling, [self.schedule]],
+            ["x", self.hx_terms, turn_off_coupling, [self.schedule]],
+        ]
+        return quspin.operators.hamiltonian(s, d, basis=self.basis, **self.checks)
+
+    def build_dlam_H(self):
+        """Method for this particular spin model to calculate
+        the $\lambda$ derivative of the Hamiltonian
+        """
+        # s = [["zz", self.J_terms], ["z", self.hz_terms], ["x", self.flipped_hx_terms]]
+        s = [["zz", self.J_terms], ["x", self.flipped_hx_terms]]
+        d = []
+        return quspin.operators.hamiltonian(s, d, basis=self.basis, **self.checks)
+
+    def build_target_H(self):
+        """Method for this particular spin model to return the target
+        Hamiltonian after annealing is complete, in the
+        most symmetric possible basis to get the ground state easier
+        """
+        s = [["zz", self.J_terms], ["z", self.hz_terms]]
+        d = []
+        return quspin.operators.hamiltonian(s, d, basis=self.targ_basis, **self.checks)
--- utils/make_dict_models.py
+++ utils/make_dict_models.py
@@ -7,14 +7,17 @@ sys.path.append(os.environ["CD_CODE_DIR"])
 
 from models.spinhalf_1d.TFIM_annealing_1d import TFIM_Annealing_1D
 from models.spinhalf_1d.LR_Ising_annealing_1d import LR_Ising_Annealing_1D
+from models.spinhalf_1d.local_field_sensing_1d import Local_Field_Sensing_1D
 
 models_dict = {
     "TFIM_1D": TFIM_Annealing_1D,
     "LR_Ising_1D": LR_Ising_Annealing_1D,
+    "Field_Sensing_1D": Local_Field_Sensing_1D,
 }
 param_names_dict = {
     "TFIM_1D": "J{0}_hx{1}",
     "LR_Ising_1D": "J{0}_hx{1}_alpha{2}",
+    "Field_Sensing_1D": "J{0}_hx{1}_hz{2}",
 }
 
 with open("{0}/dicts/models.pkl".format(os.environ["CD_CODE_DIR"]), "wb") as f:

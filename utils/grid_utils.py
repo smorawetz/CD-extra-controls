449a76a858c5ab1011ea908dba6bf8b854ca2dbb
Stewart Morawetz <stewm078@gmail.com>
2024-06-07 10:45:49 -0400

    add interp-like function for universeal fitting

--- utils/grid_utils.py
+++ utils/grid_utils.py
@@ -18,3 +18,13 @@ def get_coeffs_interp(file_sched, new_sched, file_tgrid, file_coeff_grid):
     lam_grid = file_sched.get_lam(file_tgrid)
     new_tgrid = new_sched.get_t(lam_grid)
     return interp1d(new_tgrid, file_coeff_grid.T, fill_value="extrapolate")
+
+
+def get_universal_alphas_func(coeffs_list):
+    """Return a function which always returns the same set of coefficients,
+    which are presumed to be those obtained for somme "universal" CD driving
+    Parameters:
+        alphas (np.array):  Coefficients to interpolate, has dims
+                            (grid_size, agp_order)
+    """
+    return lambda x: coeffs_list  # x will be time input, which doesn't matter

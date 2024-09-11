aa6cf324dbaec443145fb7c21868075929df06b5
Stewart Morawetz <stewm078@gmail.com>
2024-09-11 11:25:33 -0400

    MINOR: print error in universal fitting if desired

--- tools/calc_universal_fit_coeffs.py
+++ tools/calc_universal_fit_coeffs.py
@@ -11,11 +11,11 @@ from scipy.optimize import curve_fit
 with open("{0}/dicts/fit_funcs.pkl".format(os.environ["CD_CODE_DIR"]), "rb") as f:
     fit_funcs_dict = pickle.load(f)
 
-FIT_PTS = 1000  # just needs to be much larger than agp_order to ensure no fine tuning
+FIT_PTS = 10000  # just needs to be much larger than agp_order to ensure no fine tuning
 
 
 ## NOTE: currently only do linearly spaced fit, can probably improve by playing with this
-def fit_universal_coeffs(agp_order, AGPtype, min_freq, max_freq):
+def fit_universal_coeffs(agp_order, AGPtype, min_freq, max_freq, print_err=False):
     """Calculate the coefficients from trying to fit -1/x by some basis choice of odd
     polynomials in the window [window_start, window_end]
     Parameters:
@@ -27,10 +27,15 @@ def fit_universal_coeffs(agp_order, AGPtype, min_freq, max_freq):
         min_freq (float):       Point to start the fitting window of 1/x, must be > 0
         max_freq (float):       Point to end the fitting window of 1/x, assumed to
                                 be > min_freq
+        print_err (bool):       Whether or not to print squared error per datapoint
     """
     # assuming H rescaled by 1 / max_freq
     x = np.linspace(min_freq / max_freq, 1, FIT_PTS)
     y = -1 / x
     fit_func = fit_funcs_dict[AGPtype]
     opt_coeffs, _ = curve_fit(fit_func, x, y, p0=np.zeros(agp_order))
+    if print_err:
+        fit_poly = fit_func(x, *opt_coeffs)
+        error = np.sum((fit_poly - y) ** 2) / FIT_PTS
+        print(error)
     return opt_coeffs

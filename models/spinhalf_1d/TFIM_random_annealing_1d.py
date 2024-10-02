0955ec1824c1c26ae5d4a6daae458235105cb9f9
Stewart Morawetz <stewm078@gmail.com>
2024-10-02 10:59:35 -0400

    add TFIM annealing protocol with periodic disorder in real & k space

---
+++ models/spinhalf_1d/TFIM_random_annealing_1d.py
@@ -0,0 +1,103 @@
+import os
+import sys
+
+import numpy as np
+import quspin
+
+sys.path.append(os.environ["CD_CODE_DIR"])
+
+from .spinhalf_1d import SpinHalf_1D
+from tools.ham_couplings import turn_off_coupling, turn_on_coupling
+from tools.connections import neighbours_1d
+
+
+class TFIM_Random_Annealing_1D(SpinHalf_1D):
+    """Class for 1D transverse field Ising model encoding local annealing
+    problem between magnetic field polarized state at $\lambda = 0$ and
+    ferromagnetic state at $\lambda = 1$"""
+
+    def __init__(
+        self,
+        Ns,
+        H_params,
+        boundary_conds,
+        agp_order,
+        norm_type,
+        schedule,
+        symmetries={},
+        target_symmetries={},
+        rescale=1,
+    ):
+
+        J, hx, _ = map(lambda x: x * rescale, H_params)
+        Nd = H_params[2]
+        pairs = neighbours_1d(Ns, boundary_conds)
+
+        Nd = len(hi_coeffs) - 1
+        ms = np.arange(Nd + 1)
+
+        self.J_terms = [
+            [sum(Jij_coeffs * np.cos(2 * np.pi * i / 2**ms)), i, (i + 1) % Ns]
+            for i in range(Ns)
+        ]
+        self.hx_terms = [
+            [sum(hi_coeffs * np.cos(2 * np.pi * i / 2**ms)), i] for i in range(Ns)
+        ]
+        self.flipped_hx_terms = [
+            [-sum(hi_coeffs * np.cos(2 * np.pi * i / 2**ms)), i] for i in range(Ns)
+        ]
+
+        super().__init__(
+            Ns,
+            H_params,
+            boundary_conds,
+            agp_order,
+            norm_type,
+            schedule,
+            symmetries=symmetries,
+            target_symmetries=target_symmetries,
+            rescale=rescale,
+        )
+
+    def build_H0(self):
+        """Build QuSpin Hamiltonian for H0, which is component being
+        "turned on" in annealing problem"""
+        s = [["zz", self.J_terms]]
+        d = []
+        return quspin.operators.hamiltonian(s, d, basis=self.basis, **self.checks)
+
+    def build_H1(self):
+        """Method specific to this spin model to calculate
+        the bare Hamiltonian (no controls or AGP)
+        """
+        s = [["x", self.hx_terms]]
+        d = []
+        return quspin.operators.hamiltonian(s, d, basis=self.basis, **self.checks)
+
+    def build_bare_H(self):
+        """Method specific to this spin model to calculate
+        the bare Hamiltonian (no controls or AGP)
+        """
+        s = []
+        d = [
+            ["zz", self.J_terms, turn_on_coupling, [self.schedule]],
+            ["x", self.hx_terms, turn_off_coupling, [self.schedule]],
+        ]
+        return quspin.operators.hamiltonian(s, d, basis=self.basis, **self.checks)
+
+    def build_dlam_H(self):
+        """Method for this particular spin model to calculate
+        the $\lambda$ derivative of the Hamiltonian
+        """
+        s = [["zz", self.J_terms], ["x", self.flipped_hx_terms]]
+        d = []
+        return quspin.operators.hamiltonian(s, d, basis=self.basis, **self.checks)
+
+    def build_target_H(self):
+        """Method for this particular spin model to return the target
+        Hamiltonian after annealing is complete, in the
+        most symmetric possible basis to get the ground state easier
+        """
+        s = [["zz", self.J_terms]]
+        d = []
+        return quspin.operators.hamiltonian(s, d, basis=self.targ_basis, **self.checks)
--- utils/make_dict_models.py
+++ utils/make_dict_models.py
@@ -6,6 +6,7 @@ import pickle
 sys.path.append(os.environ["CD_CODE_DIR"])
 
 from models.spinhalf_1d.TFIM_annealing_1d import TFIM_Annealing_1D
+from models.spinhalf_1d.TFIM_random_annealing_1d import TFIM_Random_Annealing_1D
 from models.spinhalf_1d.LTFIM_annealing_1d import LTFIM_Annealing_1D
 from models.spinhalf_1d.TFIM_sweep_disorder_1d import TFIM_Sweep_Disorder_1D
 from models.spinhalf_1d.LR_Ising_annealing_1d import LR_Ising_Annealing_1D
@@ -15,27 +16,37 @@ from models.spinhalf_1d.local_field_sensing_1d_sweep import Local_Field_Sensing_
 from models.spinless_fermion_1d.TFIM_k_block_annealing_1d import (
     TFIM_k_Block_Annealing_1D,
 )
+from models.spinless_fermion_1d.random_TFIM_1d import TFIM_k_Block_Random_1D
+from models.spinless_fermion_1d.TFIM_fermion_annealing_1d import (
+    TFIM_Fermion_Annealing_1D,
+)
 
 from models.spinhalf_2d.disorder_Ising_2d import Disorder_Ising_2D
 
 models_dict = {
     "TFIM_1D": TFIM_Annealing_1D,
+    "TFIM_Random_1D": TFIM_Random_Annealing_1D,
     "LTFIM_1D": LTFIM_Annealing_1D,
     "TFIM_Sweep_Disorder_1D": TFIM_Sweep_Disorder_1D,
     "LR_Ising_1D": LR_Ising_Annealing_1D,
     "Field_Sensing_1D": Local_Field_Sensing_1D,
     "Field_Sensing_1D_Sweep": Local_Field_Sensing_1D_Sweep,
+    "TFIM_Fermion_1D": TFIM_Fermion_Annealing_1D,
     "TFIM_k_Block_Annealing_1D": TFIM_k_Block_Annealing_1D,
+    "TFIM_k_Block_Random_Annealing_1D": TFIM_k_Block_Random_1D,
     "Disorder_Ising_2D": Disorder_Ising_2D,
 }
 param_names_dict = {
     "TFIM_1D": "J{0}_hx{1}",
+    "TFIM_Random_1D": "J{0}_hx{1}_Nd{2}",  # J and h influence draw of params
     "LTFIM_1D": "J{0}_hx{1}_hz{2}",
     "TFIM_Sweep_Disorder_1D": "J{0}_hi{1}_hf{2}_disorder{3:.6f}_seed{4}",
     "LR_Ising_1D": "J{0}_hx{1}_alpha{2}",
     "Field_Sensing_1D": "J{0}_hx{1}_hz{2:.6f}",
     "Field_Sensing_1D_Sweep": "J{0}_hx{1}_hz{2:.6f}",
+    "TFIM_Fermion_1D": "J{0}_hx{1}_Nd{2}",
     "TFIM_k_Block_Annealing_1D": "J{0}_hx{1}",  # doesn't include k in file naming
+    "TFIM_k_Block_Random_Annealing_1D": "J{0}_hx{1}_Nd{2}",  # J and h are to draw from uniform [0, *]
     "Disorder_Ising_2D": "J{0}_hx{1}_disorder{2:.6f}_seed{3}",
 }
 
